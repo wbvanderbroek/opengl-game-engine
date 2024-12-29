049076d240d505bec4b3505dab83e78069b4e763
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2024-12-30 00:05:59 +0100

    Feat: Added color with shader

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,15 @@
 #include "VAO.h"
 #include "VBO.h"
 
-GLfloat vertices[] = {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower right corner
-	0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // upper corner
-	-0.5f / 2,  0.5f * float(sqrt(3)) / 6, 0.0f, // inner left
-	0.5f / 2,  0.5f * float(sqrt(3)) / 6, 0.0f, // inner right
-	0.0f,  -0.5f * float(sqrt(3)) / 3, 0.0f, // inner down
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
 };
-
 GLuint indices[] = {
 	0, 3, 5, // lower left triangle
 	3, 2, 4, // lower right triangle
@@ -31,8 +31,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	// Core profile, so no deprecated functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Game Engine", NULL, NULL);
 
@@ -50,7 +48,10 @@ int main()
 	glViewport(0, 0, 800, 800);
 
 
+
 	Shader shaderProgram("default.vert", "default.frag");
+
+
 
 	VAO VAO1;
 	VAO1.Bind();
@@ -58,12 +59,14 @@ int main()
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -71,6 +74,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
 
 		VAO1.Bind();
 
