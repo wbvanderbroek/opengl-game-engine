259f6d70a2c84bd834c0451318d57abe8d2b4833
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2025-03-21 12:39:01 +0100

    Chore: Added extra objects to the scene

--- Game/source/ObjectStorage.cpp
+++ Game/source/ObjectStorage.cpp
@@ -6,8 +6,32 @@
 
 ObjectStorage::ObjectStorage(Engine* engine) : m_engine(engine)
 {
-	auto model = Instantiate(Model("Assets/models/building/MetalMineBuilding.fbx"));
-	Instantiate(Model("Assets/models/character/char.fbx"));
+	auto building = Instantiate(Model("Assets/models/building/MetalMineBuilding.fbx"));
+	building->SetRotation(glm::vec3(270, 0, 180));
+	building->translation = glm::vec3(0, 0, -35);
+	building->scale = glm::vec3(1.5, 1.5, 1.5);
+
+	auto building2 = Instantiate(Model("Assets/models/building/MetalMineBuilding.fbx"));
+	building2->SetRotation(glm::vec3(270, 0, 135));
+	building2->translation = glm::vec3(35, 0, -20);
+
+	auto building3 = Instantiate(Model("Assets/models/building/MetalMineBuilding.fbx"));
+	building3->SetRotation(glm::vec3(270, 0, 225));
+	building3->translation = glm::vec3(-35, 0, -20);
+
+	auto character = Instantiate(Model("Assets/models/character/char.fbx"));
+	character->SetRotation(glm::vec3(270, 0, 0));
+	character->scale = glm::vec3(3, 3, 3);
+
+	auto plane = Instantiate(Model("Assets/models/plane/plane.fbx"));
+	plane->SetRotation(glm::vec3(270, 0, 0));
+	plane->scale = glm::vec3(300, 300, 300);
+	plane->translation = glm::vec3(0, -10, 0);
+
+
+	auto light = Instantiate(Light());
+	light->translation = glm::vec3(0, 0, -35);
+
 	Instantiate(Light());
 }
 
