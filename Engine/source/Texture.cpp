5011f7f376e07ba3350f5bd3cffbb59fb1977258
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2025-03-22 22:31:33 +0100

    Tweak: Correct indenting

--- Engine/source/Texture.cpp
+++ Engine/source/Texture.cpp
@@ -2,51 +2,53 @@
 
 Texture::Texture(const char* image, const char* texType, GLuint slot)
 {
-    type = texType;
+	type = texType;
 
-    int widthImg, heightImg, numColCh;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
 
-    glGenTextures(1, &ID);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(1, &ID);
 
-    glActiveTexture(GL_TEXTURE0 + slot);
-    unit = slot;
-    glBindTexture(GL_TEXTURE_2D, ID);
+	//fixes really really small textures
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glActiveTexture(GL_TEXTURE0 + slot);
+	unit = slot;
+	glBindTexture(GL_TEXTURE_2D, ID);
 
-    if (!bytes)
-    {
-        std::cerr << "Failed to load texture: " << image << ". Making it pink." << std::endl;
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        unsigned char pinkTexture[4 * 4] = {
-            255, 20, 147, 255,   255, 20, 147, 255,
-            255, 20, 147, 255,   255, 20, 147, 255
-        };
+	if (!bytes)
+	{
+		std::cerr << "Failed to load texture: " << image << ". Making it pink." << std::endl;
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, pinkTexture);
-    }
-    else
-    {
-        if (numColCh == 4)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-        else if (numColCh == 3)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
-        else if (numColCh == 1)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, widthImg, heightImg, 0, GL_RED, GL_UNSIGNED_BYTE, bytes);
-        else
-            std::cerr << "Texture format not supported! numColCh: " << numColCh << std::endl;
+		unsigned char pinkTexture[4 * 4] = {
+			255, 20, 147, 255,   255, 20, 147, 255,
+			255, 20, 147, 255,   255, 20, 147, 255
+		};
 
-        stbi_image_free(bytes);
-    }
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, pinkTexture);
+	}
+	else
+	{
+		if (numColCh == 4)
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+		else if (numColCh == 3)
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+		else if (numColCh == 1)
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, widthImg, heightImg, 0, GL_RED, GL_UNSIGNED_BYTE, bytes);
+		else
+			std::cerr << "Texture format not supported! numColCh: " << numColCh << std::endl;
 
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, 0);
+		stbi_image_free(bytes);
+	}
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Texture::texUnit(Shader& shader, const char* uniform, GLuint unit)
