ebf5ed33909992f8c83422865d94b41dbd81f013
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2025-04-04 09:22:36 +0200

    Refactor: Simplified release .bat

--- release.bat
+++ release.bat
@@ -24,23 +24,6 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo [INFO] Copying final build to build/release folder...
-
-REM Optional: clean or recreate release folder inside build
-if exist Release (
-    rmdir /s /q Release
-)
-mkdir Release
-
-REM Copy built EXE
-copy Release\opengl-game-engine.exe Release\
-
-REM Copy Assets folder
-xcopy ..\Assets Release\Assets /E /I /Y >nul
-
-REM Copy DLLs
-copy ..\Libraries\dll\assimp-vc143-mt.dll Release\ >nul
-
-echo [INFO] Release build prepared in build/Release
+echo [INFO] Release build completed successfully.
 
 endlocal
